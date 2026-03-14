a4519d9a87679ad0d80a0ae4162f3841be2b63e9
Jacob Vidal <firstcommand@gmail.com>
2026-03-14 20:09:12 -0300

    change main.py to read the dynamic string from ssm parameter store

--- app/main.py
+++ app/main.py
@@ -1,20 +1,23 @@
 import os
-from dotenv import load_dotenv
+import boto3
+
 from fastapi import FastAPI, Request, status
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
+from mangum import Mangum
 
-load_dotenv()
+AWS_REGION = os.getenv("AWS_REGION", "us-east-2")
+TEMPLATE_DIR = os.path.join(os.path.dirname(__file__), "templates")
 
 app = FastAPI()
-templates = Jinja2Templates(directory="app/templates")
+templates = Jinja2Templates(directory=TEMPLATE_DIR)
+ssm = boto3.client("ssm", region_name=AWS_REGION)
 
 
 def get_dynamic_string():
-    value = os.getenv("DYNAMIC_STRING")
-    if not value:
-        raise RuntimeError("DYNAMIC_STRING is not set. Add it to your .env file.")
-    return value
+    param_name = os.environ.get("PARAMETER_NAME", "/merapar/dynamic_string")
+    parameter = ssm.get_parameter(Name=param_name, WithDecryption=False)
+    return parameter["Parameter"]["Value"]
 
 
 @app.get("/", include_in_schema=False)
@@ -31,3 +34,5 @@ async def read_item(request: Request):
     return templates.TemplateResponse(
         "index.html", {"request": request, "dynamic_string": current_value}
     )
+
+handler = Mangum(app)
